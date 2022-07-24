d830b64f3d769cbfbd412b38a7038d9d9f904751
Elbarae Rguig <elbarae1921@gmail.com>
2022-07-24 15:53:09 +0100

    innit to win it

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.html',
+    './src/**/*.html',
+    "./src/**/*.css",
+    "./src/**/*.scss",
+    "./src/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
